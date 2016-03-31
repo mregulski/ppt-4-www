4783e4695115f8368d2b03062f7bb70d8f7e2037
Marcin Regulski <maarcin.regulski@gmail.com>
2016-03-31 22:54:59 +0200

    add mergeSort visualisation

--- algo/scripts/menu.js
+++ algo/scripts/menu.js
@@ -1,11 +1,54 @@
-function toggleMenu() {
-    var el = document.getElementById("menu");
-    if(el.style.display != 'block')
-        el.style.display = 'block';
-    else
-        el.style.display = 'none';
-}
+page = (function(){
+    'use strict';
+    var menu = null;
+    var input = null;
+    var run = null;
+    window.onload = function() {
+        menu = document.getElementById("menu");
+        input = document.getElementById("array");
+        run = document.getElementById("runButton");
 
-window.onload = function() {
-    document.getElementById("menuToggle").onclick = toggleMenu;
-}
+        document.getElementById("menuToggle").onclick = toggleMenu;
+        run.addEventListener('click', runSort);
+        input.addEventListener('keypress', checkInput);
+    }
+
+    // re-enable hidden menu when window is resized above breakpoint
+    var mq = window.matchMedia("(min-width: 680px)");
+    mq.addListener(enableMenu);
+
+
+    function checkInput() {
+        var array = input.value.split(/[,; ]/);
+        //console.log(array);
+        if(array.length > 1) {
+            run.disabled = false;
+        }
+        else {
+            run.disabled = true;
+        }
+    }
+
+    function enableMenu(mediaQuery) {
+        if(mediaQuery.matches) {
+            menu.style.display = 'block';
+        }
+    }
+
+    function runSort() {
+        var array = input.value.split(/[,; ]/);
+        console.log(array);
+        array = array.map(function(x) {
+            return parseInt(x,10);
+        })
+        console.log(sort.mergeSort(array));
+    }
+
+    function toggleMenu() {
+        if(menu.style.display != 'block')
+            menu.style.display = 'block';
+        else
+            menu.style.display = 'none';
+    }
+
+})();
---
+++ algo/scripts/mergeSort.js
@@ -0,0 +1,95 @@
+sort = (function() {
+    var output = undefined;
+    return {
+        mergeSort: mergeSort
+    };
+
+    function mergeSort(array) {
+        if(output === undefined) {
+            output = document.getElementById("output");
+        }
+        while(output.firstChild) {
+            output.removeChild(output.firstChild);
+        }
+        return _mergeSort(array, 0);
+    }
+    function _mergeSort(array, level) {
+
+
+        if (array.length <= 1) {
+            return array;
+        }
+
+        var left = [];
+        var right = [];
+        for (var i = 0; i < array.length; i++) {
+            if(i%2 == 0) {
+                left.push(array[i]);
+            }
+            else {
+                right.push(array[i]);
+            }
+        }
+        log("split " + array, "split", level);
+        log("->  left: " + left, "split", level);
+        log("-> right: " + right, "split", level);
+
+        console.log("split:")
+        console.log("\tleft: " + left);
+        console.log("\tright: " + right);
+        left = _mergeSort(left, level+1);
+        right = _mergeSort(right, level+1);
+        return merge(left, right, level);
+    }
+
+    function merge(left, right, level) {
+        var output = document.getElementById("output");
+        var result = [];
+        log("merging","merge", level);
+        log("    left: " + left, "merge", level);
+        log(" & right: " + right, "merge", level);
+
+        console.log("merge:")
+        console.log("    left: " + left);
+        console.log("  &right: " + right);
+        while (left.length > 0 && right.length > 0) {
+            if(left[0] <= right[0]) {
+                result.push(left.shift());
+            }
+            else {
+                result.push(right.shift());
+            }
+
+        }
+        while (left.length > 0) {
+            result.push(left.shift());
+        }
+
+        while (right.length > 0) {
+            result.push(right.shift());
+        }
+        log("    -> result: " + result, "merge", level);
+        console.log("\tresult: " + result);
+        return result;
+    }
+
+    function log(text, step, level) {
+        var br = document.createElement("br");
+        var el = document.createElement("span");
+        if(step == "split")
+        {
+            var txt = document.createTextNode('-'.repeat(2*level)+text);
+
+        }
+        else if (step == "merge") {
+            var txt = document.createTextNode('-'.repeat(2*(level)) + text);
+        }
+        else {
+            return;
+        }
+        el.appendChild(txt);
+        output.appendChild(el);
+        output.appendChild(br);
+    }
+
+})();
